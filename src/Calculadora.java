6d71c944b9ec0fd194be87fc21e3a263dd31d92f
cCaio-byte <62912809+cCaio-byte@users.noreply.github.com>
2021-08-04 21:23:31 -0300

    added commets

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
+
 
 public class Calculadora {
   private float a, b, c;
   private float num1, num2;
+
+  /* Construiu-se dois contrututores para a classe Calculadora, em que um recebe tres floats como parametro para 
+  a calculadora de raízes, sendo os coeficientes da equacao, e o outro recebe apenas dois floats para a calculadora basica*/
   
   public Calculadora(float a, float b, float c) {
     this.a = a;
@@ -41,96 +44,22 @@ public class Calculadora {
     double x1 = (-b + Math.sqrt(delta)) / (2 * a);
     double x2 = (-b - Math.sqrt(delta)) / (2 * a);
 
+    // Analisa o delta da equacao. Caso seja menor do que 0, nao havera raizes reais. 
     if(delta < 0){
       System.out.println("Não há raízes reais");
 
       return null;
     }else 
+    // Analisa o delta da equacao. Caso seja igual a 0, havera apenas uma raiz real. 
     if (delta == 0){
-      System.out.println("Há uma raiz real: " + x1);
+      System.out.println("\nHá uma raiz real: " + x1);
 
       return null;
     }
     
+    // a funcao retorna o array com as duas raizes da equaçao.
     return new double[]{x1, x2};
   }
   
-  public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    boolean execution = true;
-    
-    while (execution) {
-      System.out.println("Digite a opção desejada:\n0 - Sair do programa\n1 - Calculadora básica\n2 - Calculadora de raízes");
-      int option = sc.nextInt();
-      
-      switch (option) {
-        case 0:
-          execution = false;
-          break;
-
-        case 1:
-          System.out.print("Digite o primeiro valor: ");
-          float num1 = sc.nextFloat();
-
-          System.out.print("Digite o segundo valor: ");
-          float num2 = sc.nextFloat();
-
-          System.out.println("Digite a operação desejada: ");
-          System.out.println("1 - Soma");
-          System.out.println("2 - Subtração");
-          System.out.println("3 - Multiplicação");
-          System.out.println("4 - Divisão");
-          System.out.println("5 - Modulo");
-
-          int option2 = sc.nextInt();
-
-          Calculadora calc = new Calculadora(num1, num2);
-
-          float result = 0;
-
-          switch (option2) {
-            case 1:
-              result = calc.soma();
-              break;
-            case 2:
-              result = calc.subtracao();
-              break;
-            case 3:
-              result = calc.multiplicacao();
-              break;
-            case 4:
-              result = calc.divisao();
-              break;
-            case 5:
-              result = calc.modulo();
-              break;
-            default:
-              System.out.println("Opção inválida!");
-              break;
-          }
-
-          System.out.println("Resultado: "+ result +"\n");
-          break;
-          
-        case 2:
-          System.out.print("Digite o valor de a: ");
-          float a = sc.nextFloat();
-          System.out.print("Digite o valor de b: ");
-          float b = sc.nextFloat();
-          System.out.print("Digite o valor de c: ");
-          float c = sc.nextFloat();
-
-          Calculadora calculadora = new Calculadora(a, b, c);
-          double[] raizes = calculadora.calculaRaizes();
-
-          if (raizes != null) {
-            System.out.println("Raízes da equação: " + raizes[0] + " e " + raizes[1] + "\n");
-          }
-          break;
-        default:
-          System.out.println("Opção inválida!");
-          break;
-      }
-    }
-  }
+ 
 }
---
+++ src/Principal.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        boolean execution = true;
+        
+        // o while e utilizado para mantar a caluladora aberta ate que o usuario passe o valor false para a variavel execution, passando o valor 0 no menu inicial de opcoes da calculadora. 
+        while (execution) {
+          System.out.println("\nDigite a opção desejada:\n0 - Sair do programa\n1 - Calculadora básica\n2 - Calculadora de raízes");
+          int option = sc.nextInt();
+          
+          /* A partir da funcao switch determina-se tres possibilidades de funcionamento do programa, alem de uma ultima posssibilidade 
+          para quando um valor de entrada invalido e passado pelo usuario. */
+          switch (option) {
+              // Nesta primeira possibilidade o usuario fecha o programa
+            case 0:
+              execution = false;
+              break;
+              // Nesta segunda possibilidade inicia-se a calculadora basica
+            case 1:
+              System.out.print("Digite o primeiro valor: ");
+              float num1 = sc.nextFloat();
+    
+              System.out.print("Digite o segundo valor: ");
+              float num2 = sc.nextFloat();
+    
+              System.out.println("Digite a operação desejada: ");
+              System.out.println("1 - Soma");
+              System.out.println("2 - Subtração");
+              System.out.println("3 - Multiplicação");
+              System.out.println("4 - Divisão");
+              System.out.println("5 - Modulo");
+    
+              int option2 = sc.nextInt();
+    
+              Calculadora calc = new Calculadora(num1, num2);
+    
+              float result = 0;
+
+              /* Nesta funcao switch, dentro da calculadora basica, 5 opcoes sao possiveis para usuario. Dependendo da entrada do usuario, uma funcao da classe calculadora e chamada para realizar uma opercao: soma, subtracao, multiplicacao, divisao ou resto da divisao. Alem de uma ultimo caso, em que uma opcao invalida e passada. */ 
+              switch (option2) {
+                case 1:
+                  result = calc.soma();
+                  break;
+                case 2:
+                  result = calc.subtracao();
+                  break;
+                case 3:
+                  result = calc.multiplicacao();
+                  break;
+                case 4:
+                  result = calc.divisao();
+                  break;
+                case 5:
+                  result = calc.modulo();
+                  break;
+                default:
+                  System.out.println("Opção inválida!");
+                  break;
+              }
+    
+              System.out.println("Resultado: "+ result +"\n");
+              break;
+
+              /* Nesta terceira possibilidade inicia-se a calculadora de raizes, na qual o usuario deve passar os tres coeficientes da equacao
+              do segundo grau para a obtencao das raizes. */
+            case 2:
+              System.out.print("Digite o valor de a: ");
+              float a = sc.nextFloat();
+              System.out.print("Digite o valor de b: ");
+              float b = sc.nextFloat();
+              System.out.print("Digite o valor de c: ");
+              float c = sc.nextFloat();
+    
+              Calculadora calculadora = new Calculadora(a, b, c);
+              double[] raizes = calculadora.calculaRaizes();
+    
+              if (raizes != null) {
+                System.out.println("Raízes da equação: " + raizes[0] + " e " + raizes[1] + "\n");
+              }
+              break;
+            default:
+              System.out.println("Opção inválida!");
+              break;
+          }
+        }
+      }
+}
